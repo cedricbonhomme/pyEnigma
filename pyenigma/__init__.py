61cf92654e772cad2e51e6eced374ec99cb72f59
C?dric Bonhomme <kimble.mandel@gmail.com>
2013-02-03 13:48:54 +0100

    added __init__.py

---
+++ pyenigma/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+#-*- coding: utf-8 -*-
+
+
+__author__ = "Christophe Goessen, Cedric Bonhomme"
+__version__ = "0.1"
+__date__ = "$Date: 2010/01/29 $"
+__revision__ = "$Date: 2013/02/02 $"
+__copyright__ = "Copyright (c) Christophe Goessen, Cedric Bonhomme"
+__license__ = "GPLv3"
