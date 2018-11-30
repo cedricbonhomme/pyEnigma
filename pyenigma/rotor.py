ccd2cd97a755210392f2af2a924c346f0826db6c
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-11-30 23:24:14 +0100

    removed useless function

--- pyenigma/rotor.py
+++ pyenigma/rotor.py
@@ -65,9 +65,6 @@ class Rotor(object):
         self.date = date
         self.state = state
 
-    def __getattr__(self, name):
-        return self.__dict__[name]
-
     def __setattr__(self, name, value):
         self.__dict__[name] = value
         if name == 'wiring':
