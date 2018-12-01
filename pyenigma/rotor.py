303ce4836c439b3a300455e0b5e729a5ec9f573b
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-12-01 01:11:33 +0100

    Removed useless function.

--- pyenigma/rotor.py
+++ pyenigma/rotor.py
@@ -14,9 +14,6 @@ class Reflector(object):
         self.model = model
         self.date = date
 
-    def __getattr__(self, name):
-        return self.__dict__[name]
-
     def __setattr__(self, name, value):
         self.__dict__[name] = value
 
